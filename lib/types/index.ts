a221453a3f60fb7b70bd287d3d9dc6fc172f6887
JoseMarshall <hebojosemar@gmail.com>
2022-06-04 01:09:26 +0100

    refactor(addlogger): allow formatting message

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -77,16 +77,21 @@ export type InitialConfig = {
   file?: FileConfig;
 };
 
+export type FormatOptions = {
+  prettify: boolean;
+};
+
 export type PluginConfig = {
-  silent: boolean,
-  level: `${LogLevel}`,
-  [property: string]: any
-}
+  silent: boolean;
+  level: `${LogLevel}`;
+  format?: FormatOptions;
+  [property: string]: any;
+};
 
 export type LoggerPlugin = {
-  [property: string]: any
-  [LogLevel.debug]: (content: any) => void,
-  [LogLevel.info]: (content: any) => void,
-  [LogLevel.warning]: (content: any) => void,
-  [LogLevel.error]: (content: any) => void,
-}
+  [property: string]: any;
+  [LogLevel.debug]: (content: any) => void;
+  [LogLevel.info]: (content: any) => void;
+  [LogLevel.warning]: (content: any) => void;
+  [LogLevel.error]: (content: any) => void;
+};
